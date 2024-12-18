20f0cce5353a9e1f3c74e80c8bb128b9cc16a977
Kürşat Tokpınar <kursattokpinar@gmail.com>
2024-12-18 11:13:13 +0300

    Fix doc

--- src/models/generic-list-entity-relation.model.ts
+++ src/models/generic-list-entity-relation.model.ts
@@ -4,7 +4,7 @@ import {Entity, model, property} from '@loopback/repository';
   settings: {
     strict: false,
     mongodb: {
-      collection: "GenericListToEntityRelation"
+      collection: process.env.collection_list_list_entity_rel ?? "GenericListToEntityRelation"
     }
   }
 })
