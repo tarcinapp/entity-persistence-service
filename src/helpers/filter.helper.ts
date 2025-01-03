56c7ebc1aa6a72ee3614c69a6f624768da92bd2a
Kürşat Tokpınar <kursattokpinar@gmail.com>
2025-01-03 19:08:49 +0000

    Bugfix for sanitize filters

--- src/helpers/filter.helper.ts
+++ src/helpers/filter.helper.ts
@@ -1,14 +1,31 @@
 import {Fields} from '@loopback/repository';
-import {mapValues} from 'lodash';
+import {isPlainObject, mapValues} from 'lodash';
 
 // Helper function to ensure boolean values in 'fields'
-export function sanitizeFilterFields(filter?: {fields?: Fields}) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function sanitizeFilterFields(filter?: any): void {
+  if (!filter || typeof filter !== 'object') return;
 
-  if (filter?.fields) {
+  // Check if the current object has a `fields` property
+  if (filter.fields) {
     filter.fields = mapValues(filter.fields, value =>
       typeof value === 'string' && value === 'true' ? true :
         typeof value === 'string' && value === 'false' ? false :
-          value  // Keep the original value if it's not 'true' or 'false'
+          value // Keep the original value if it's not 'true' or 'false'
     ) as Fields;
+  }
+
+  // Recursively sanitize any nested objects
+  for (const key of Object.keys(filter)) {
+    if (isPlainObject(filter[key]) || Array.isArray(filter[key])) {
+      if (Array.isArray(filter[key])) {
+        // For arrays, apply recursively to each element
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        filter[key].forEach((item: any) => sanitizeFilterFields(item));
+      } else {
+        // For objects, apply recursively
+        sanitizeFilterFields(filter[key]);
+      }
+    }
   }
 }
