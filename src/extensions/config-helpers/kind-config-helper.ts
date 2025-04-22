c583b5850e169eaa04e7392d8b0c40e620b7cdd6
Kursat Tokpunar <kadir.kursat.tokpunar@ibm.com>
2025-04-22 13:23:28 +0300

    Add reactions kind configs

--- src/extensions/config-helpers/kind-config-helper.ts
+++ src/extensions/config-helpers/kind-config-helper.ts
@@ -18,16 +18,22 @@ export class KindConfigurationReader {
   private isKindConfiguredForEntities: boolean = false;
   private isKindConfiguredForLists: boolean = false;
   private isKindConfiguredForListEntityKinds: boolean = false;
+  private isKindConfiguredForEntityReactions: boolean = false;
+  private isKindConfiguredForListReactions: boolean = false;
 
   // Allowed kinds arrays
   private allowedKindsForEntitiesArr: string[] = [];
   private allowedKindsForListsArr: string[] = [];
   private allowedKindsForEntityListRelationsArr: string[] = [];
+  private allowedKindsForEntityReactionsArr: string[] = [];
+  private allowedKindsForListReactionsArr: string[] = [];
 
   // Default kinds
   private readonly defaultEntityKindValue: string = 'entity';
   private readonly defaultListKindValue: string = 'list';
   private readonly defaultRelationKindValue: string = 'relation';
+  private readonly defaultEntityReactionKindValue: string = 'reaction';
+  private readonly defaultListReactionKindValue: string = 'reaction';
 
   constructor() {
     this.initKindConfigurations();
@@ -38,9 +44,13 @@ export class KindConfigurationReader {
     this.isKindConfiguredForEntities = false;
     this.isKindConfiguredForLists = false;
     this.isKindConfiguredForListEntityKinds = false;
+    this.isKindConfiguredForEntityReactions = false;
+    this.isKindConfiguredForListReactions = false;
     this.allowedKindsForEntitiesArr = [];
     this.allowedKindsForListsArr = [];
     this.allowedKindsForEntityListRelationsArr = [];
+    this.allowedKindsForEntityReactionsArr = [];
+    this.allowedKindsForListReactionsArr = [];
     this.initKindConfigurations();
   }
 
@@ -56,6 +66,14 @@ export class KindConfigurationReader {
     return this.allowedKindsForEntityListRelationsArr;
   }
 
+  public get allowedKindsForEntityReactions() {
+    return this.allowedKindsForEntityReactionsArr;
+  }
+
+  public get allowedKindsForListReactions() {
+    return this.allowedKindsForListReactionsArr;
+  }
+
   public get defaultEntityKind() {
     return this.defaultEntityKindValue;
   }
@@ -66,6 +84,14 @@ export class KindConfigurationReader {
 
   public get defaultRelationKind() {
     return this.defaultRelationKindValue;
+  }
+
+  public get defaultEntityReactionKind() {
+    return this.defaultEntityReactionKindValue;
+  }
+
+  public get defaultListReactionKind() {
+    return this.defaultListReactionKindValue;
   }
 
   private initKindConfigurations() {
@@ -97,6 +123,32 @@ export class KindConfigurationReader {
         this.defaultRelationKindValue,
       ];
     }
+
+    if (_.has(process.env, 'entity_reaction_kinds')) {
+      this.isKindConfiguredForEntityReactions = true;
+
+      const kinds = _.split(process.env['entity_reaction_kinds'], ',').map(
+        _.trim,
+      );
+
+      this.allowedKindsForEntityReactionsArr = [
+        ...kinds,
+        this.defaultEntityReactionKindValue,
+      ];
+    }
+
+    if (_.has(process.env, 'list_reaction_kinds')) {
+      this.isKindConfiguredForListReactions = true;
+
+      const kinds = _.split(process.env['list_reaction_kinds'], ',').map(
+        _.trim,
+      );
+
+      this.allowedKindsForListReactionsArr = [
+        ...kinds,
+        this.defaultListReactionKindValue,
+      ];
+    }
   }
 
   public isKindAcceptableForEntity(kind: string) {
@@ -118,6 +170,22 @@ export class KindConfigurationReader {
   public isKindAcceptableForListEntityRelations(kind: string) {
     if (this.isKindConfiguredForListEntityKinds) {
       return this.allowedKindsForEntityListRelationsArr.includes(kind);
+    }
+
+    return true;
+  }
+
+  public isKindAcceptableForEntityReactions(kind: string) {
+    if (this.isKindConfiguredForEntityReactions) {
+      return this.allowedKindsForEntityReactionsArr.includes(kind);
+    }
+
+    return true;
+  }
+
+  public isKindAcceptableForListReactions(kind: string) {
+    if (this.isKindConfiguredForListReactions) {
+      return this.allowedKindsForListReactionsArr.includes(kind);
     }
 
     return true;
